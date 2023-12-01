c453509bae4a400d686b9d7ebd761c6e8e5610e9
Andrew J. Mauer <ajmauer@gmail.com>
2023-12-01 01:51:54 +0000

    move common projects to central location

--- buildAll.sh
+++ buildAll.sh
@@ -1,2 +1,4 @@
+cd common.jdt && ./mvnw clean install && cd .. && \
+cd common.ls && ./mvnw clean install && cd .. && \
 cd microprofile.jdt && ./mvnw clean install && cd .. && \
 cd microprofile.ls/org.eclipse.lsp4mp.ls && ./mvnw clean install && cd ../.. 
